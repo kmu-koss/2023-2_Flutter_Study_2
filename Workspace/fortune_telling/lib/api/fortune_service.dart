113ee7ab3a73a83e7a57ad6c5cd02f9594d57da5
SunwooKim11 <seonu2001@naver.com>
2023-11-23 00:17:51 +0900

    Debugging API
    - Json 구조, `jsonDecode(dataString)` 의 구조가 아래와 같음.
        - `//  Map<STring, Map<String, Map<String, dynamic>>>`
    - Error Message
        - I/flutter (31850): Error fetching or decoding daily fortunes: type '_Map<String, dynamic>' is not a subtype of type 'Map<String, Map<String, Map<String, dynamic>>>’
    
    - dynamic, jsonDecode을 공부 필요.
    - 필요하다면, jsonDecode을 여러번 활용하여 nested json decode 문제를 풀어야할듯

--- Workspace/fortune_telling/lib/api/fortune_service.dart
+++ Workspace/fortune_telling/lib/api/fortune_service.dart
@@ -1,42 +1,55 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:flutter/services.dart';
 import '../model/model.dart';
 
-// Mock recipe service that grabs sample json data to mock recipe request/response
+// Mock fortune service that grabs sample json data to mock recipe request/response
 class FortuneService {
-  // Batch request that gets both today recipes and friend's feed
+  // Batch request that gets both today fortune and friend's feed
   Future<ExploreData> getExploreData() async {
     final dailyFortunes = await _getDailyFortunes();
     // todo implement constellation
+    // then
     // final constellationFortunes = await _getConstellation();
 
     return ExploreData(dailyFortunes);
     // return ExploreData(dailyFortnes, constellationFortunes);
   }
 
-  // Get sample explore recipes json to display in ui
+  // Get sample explore fortunes json to display in ui
   Future<List<DynamicDailyFortunes>> _getDailyFortunes() async {
+    try {
+      // Simulate api request wait time
+      await Future.delayed(const Duration(milliseconds: 1000));
+      // todo get json from internet
+      // Load json from file system
+      final dataString =
+      await _loadAsset('assets/sample_data/sample_daily_fortune.json');
+      // debugPrint(dataString);
+      // Decode to json
+      // todo to solve
+      Map<String, Map<String, Map<String, dynamic>>> json =
+          jsonDejcode(dataString);
+      // Go through each fortune and convert json to ExploreFortune object.
 
-    // Simulate api request wait time
-    await Future.delayed(const Duration(milliseconds: 1000));
-    // todo get json from internet
-    // Load json from file system
-    final dataString =
-    await _loadAsset('assets/sample_data/sample_daily_fortune.json');
+      // final Map<String, Object> tmp = json['result'] as Map<String, Object>;
 
-    // Decode to json
-    final Map<String, dynamic> json = jsonDecode(dataString);
-    // Go through each recipe and convert json to ExploreRecipe object.
-    if (json['result'] !=  null) {
-      print(json['result']);
-      final dailyFortunes = <DynamicDailyFortunes>[];
-      json['result'].forEach((v) {
-        dailyFortunes.add(DynamicDailyFortunes.fromJson(v));
-      });
-      return dailyFortunes;
-    } else {
+      if (json != null &&  json['result'] !=null) {
+        // debugPrint(json['result']?['day']);
+        final _dailyFortunes = <DynamicDailyFortunes>[];
+        json['result']?.forEach((key, value) {
+          _dailyFortunes.add(DynamicDailyFortunes.fromJson(value));
+        });
+        // debugPrint(json['result']['day']['title']);
+        return _dailyFortunes;
+      } else {
+        debugPrint('Error: JSON is null or missing "result" key.');
+        return [];
+      }
+    } catch (e) {
+      debugPrint('Error fetching or decoding daily fortunes: $e');
       return [];
     }
   }
@@ -62,8 +75,6 @@ class FortuneService {
   //     return [];
   //   }
   // }
-
-
 
   // Loads sample json data from file system
   Future<String> _loadAsset(String path) async {
